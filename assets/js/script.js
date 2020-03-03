90b8acc99908cf945a9d45ea1108022e139cedf5
Kurt Yazdizadeh <kurt.yazdizadeh@gmail.com>
2020-03-03 14:51:28 -0800

    Added card flip functionality w/ click handler in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
+var gameCards = document.getElementById('gameCards');
 
+function handleClick(event) {
+  if (event.target.className.indexOf('card-back') === -1){
+    return;
+  }
+  event.target.classList.add('hidden');
+}
+
+gameCards.addEventListener('click', function(){
+  handleClick(event)
+});

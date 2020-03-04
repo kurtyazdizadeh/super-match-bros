89662f37c0777d26eca870f8a0f0e7d2c7d6b012
Kurt Yazdizadeh <kurt.yazdizadeh@gmail.com>
2020-03-03 16:04:53 -0800

    Memory Match - Match Two Cards Functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,40 @@
-var gameCards = document.getElementById('gameCards');
+var firstCardClicked,
+    firstCardClasses,
+    secondCardClicked,
+    secondCardClasses,
+    gameCards = document.getElementById('gameCards');
 
 function handleClick(event) {
   if (event.target.className.indexOf('card-back') === -1){
     return;
   }
+
   event.target.classList.add('hidden');
+
+
+  if (!firstCardClicked) {
+    firstCardClicked = event.target;
+    firstCardClasses = firstCardClicked.previousElementSibling.className;
+  } else if (firstCardClicked) {
+    secondCardClicked = event.target;
+    secondCardClasses = secondCardClicked.previousElementSibling.className;
+
+    gameCards.removeEventListener('click', handleClick);
+
+    if (firstCardClasses === secondCardClasses) {
+      gameCards.addEventListener('click', handleClick);
+      firstCardClicked = null;
+      secondCardClicked = null;
+    } else {
+      setTimeout(() => {
+        firstCardClicked.classList.remove('hidden');
+        secondCardClicked.classList.remove('hidden');
+        gameCards.addEventListener('click', handleClick);
+        firstCardClicked = null;
+        secondCardClicked = null;
+      }, 1500);
+    }
+  }
 }
 
-gameCards.addEventListener('click', function(){
-  handleClick(event)
-});
+gameCards.addEventListener('click', handleClick);
